3936cb61b46f864cf0ddf35fa14b74d0299e9c84
EpicTeddy101 <deakinm04@gmail.com>
2025-03-08 13:39:58 +1100

    implemented basic light/dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
     ({
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: true,
+        disableSwitch: false,
         respectPrefersColorScheme: false
       },
       // Replace with your project's social card
